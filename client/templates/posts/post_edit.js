a2b74aa9ecadf76256f72130f22bbed95993d523
kevin1884 <kevinzh1884@gmail.com>
2015-08-04 20:56:17 -0700

    Added error checking system to microscope.

--- client/templates/posts/post_edit.js
+++ client/templates/posts/post_edit.js
@@ -2,6 +2,19 @@
  * Created by xzhang on 8/2/15.
  */
 
+Template.postEdit.onCreated(function() {
+    Session.set('postEditErrors', {});
+});
+
+Template.postEdit.helpers({
+    errorMessage: function(field) {
+        return Session.get('postEditErrors')[field];
+    },
+    errorClass: function(field) {
+        return !!Session.get('postEditErrors')[field] ? 'has-error' : '';
+    }
+});
+
 Template.postEdit.events({
     'submit form': function(e) {
         e.preventDefault();
@@ -13,9 +26,14 @@ Template.postEdit.events({
             title: $(e.target).find('[name=title]').val()
         };
 
+        var errors = validatePost(postProperties);
+        if (errors.url || errors.title) {
+            return Session.set('postEditErrors', errors);
+        }
+
         Posts.update(currentPostId, {$set: postProperties}, function(error) {
             if (error) {
-                alert(error.reason);
+                throwError(error.reason);
             } else {
                 Router.go('postPage', {_id: currentPostId});
             }
